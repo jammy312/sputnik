d1329d1ddbed2716bac6b7199a614800a110be1b
Bintousk <senibintou@gmail.com>
2024-02-08 15:48:58 -0500

    add test but in comment

---
+++ src/test/java/pl/touk/sputnik/connector/saas/ConnectorTypeTest.java
@@ -0,0 +1,30 @@
+package pl.touk.sputnik.connector.saas;
+
+import org.junit.jupiter.api.Test;
+import pl.touk.sputnik.configuration.Configuration;
+import pl.touk.sputnik.connector.ConnectorType;
+import pl.touk.sputnik.connector.gerrit.GerritFacade;
+import pl.touk.sputnik.connector.gerrit.GerritFacadeBuilder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ConnectorTypeTest {
+    @Test
+    void shouldReturnConnectorType(){
+        String goodName = "github";
+        assertThat(ConnectorType.getValidConnectorType(goodName)).isEqualTo(ConnectorType.GITHUB);
+    }
+
+    @Test
+    void shouldReturnGerritByDefault(){
+        assertThat(ConnectorType.getValidConnectorType(null)).isEqualTo(ConnectorType.GERRIT);
+    }
+
+    @Test
+    void shouldReturnGerritWhenWrongName(){
+        String wrongName = "wrongName";
+        assertThat(ConnectorType.getValidConnectorType(wrongName)).isEqualTo(ConnectorType.GERRIT);
+    }
+}

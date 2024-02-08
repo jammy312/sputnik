0b9536bf8143ffc9999d15fc4b5f449534b3954d
Bintousk <senibintou@gmail.com>
2024-02-08 15:49:56 -0500

    add tests for Connector Type

--- src/test/java/pl/touk/sputnik/connector/saas/ConnectorTypeTest.java
+++ src/test/java/pl/touk/sputnik/connector/saas/ConnectorTypeTest.java
@@ -11,6 +11,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class ConnectorTypeTest {
+
     @Test
     void shouldReturnConnectorType(){
         String goodName = "github";
